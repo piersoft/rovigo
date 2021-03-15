5f306fb1ddb15ebf37398baf9eaafc56dd7cfe44
Francesco Piero Paolicelli <piersoft2@gmail.com>
2021-03-15 13:22:26 +0100

    Add files via upload

---
+++ asteroideb167.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <meta property="og:image" content="http://www.piersoft.it/trasportilecce/bus_.png"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+       <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet" type="text/css">
+<style>
+body,td,th {
+font-family: "Titillium Web, Arial, Sans-Serif";
+font-size: 14px;
+}
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
+<style>
+#logo{
+top:50px;
+  padding: 10px 15px 10px 15px;
+}
+.smart-green {
+    margin-left:auto;
+    margin-right:auto;
+
+    max-width: 300px;
+    background: #F8F8F8;
+    padding: 30px 30px 20px 30px;
+    font: 12px "Titillium Web",Arial, Helvetica, sans-serif;
+    color: #666;
+    border-radius: 5px;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+}
+.smart-green h1 {
+    font: 24px "Titillium Web", Arial, Helvetica, sans-serif;
+    padding: 20px 0px 20px 40px;
+    display: block;
+    margin: -30px -30px 10px -30px;
+    color: #FFF;
+    background: #9DC45F;
+    text-shadow: 1px 1px 1px #949494;
+    border-radius: 5px 5px 0px 0px;
+    -webkit-border-radius: 5px 5px 0px 0px;
+    -moz-border-radius: 5px 5px 0px 0px;
+    border-bottom:1px solid #89AF4C;
+
+}
+.smart-green h1>span {
+    display: block;
+    font-size: 11px;
+    color: #FFF;
+}
+
+.smart-green label {
+    display: block;
+    margin: 0px 0px 5px;
+}
+.smart-green label>span {
+    float: left;
+    margin-top: 10px;
+    color: #5E5E5E;
+}
+.smart-green input[type="text"], .smart-green input[type="email"], .smart-green textarea, .smart-green select {
+    color: #555;
+    height: 20px;
+    line-height:15px;
+    width: 20%;
+    padding: 10px 0px 10px 10px;
+    margin-left: 1px;
+    border: 1px solid #E5E5E5;
+    background: #FBFBFB;
+    outline: 0;
+    -webkit-box-shadow: inset 1px 1px 2px rgba(238, 238, 238, 0.2);
+    box-shadow: inset 1px 1px 2px rgba(238, 238, 238, 0.2);
+    font: "Titillium Web",Arial, Helvetica, sans-serif;
+      border-radius: 5px;
+        font-size: 24px;
+}
+.smart-green textarea{
+    height:100px;
+    padding-left:     10px;
+
+}
+.smart-green select {
+    background: url('down-arrow.png') no-repeat right, -moz-linear-gradient(top, #FBFBFB 0%, #E9E9E9 100%);
+    background: url('down-arrow.png') no-repeat right, -webkit-gradient(linear, left top, left bottom, color-stop(0%,#FBFBFB), color-stop(100%,#E9E9E9));
+   appearance:none;
+    -webkit-appearance:none;
+   -moz-appearance: none;
+    text-indent: 0.01px;
+    text-overflow: '';
+    width:100%;
+    height:30px;
+}
+.smart-green .button {
+    background-color: #9DC45F;
+    border-radius: 5px;
+    -webkit-border-radius: 5px;
+    -moz-border-border-radius: 5px;
+    border: none;
+    padding: 10px 15px 10px 15px;
+    color: #FFF;
+    font-size: 15px;
+    text-shadow: 1px 1px 1px #949494;
+}
+.smart-green .button:hover {
+    background-color:#80A24A;
+}
+
+  </style>
+  <form action="http://dati.comune.lecce.it/trasporti/tmp167.php?id=" method="GET" class="smart-green">
+
+   <p><h1>Numero fermata:   <input type="text" name="id" value ="" /></p><span><h3>Inserire il numero presente sulla palina. Esempio: 137</h3></span></h1>
+   <p style="line-height: 30px; width: 300px;"><h2><input class="button" type="submit" style="line-height: 30px; width: 300px;"></h2>
+  </form>
+
+</form>
+
+ </body>
+       </html>
